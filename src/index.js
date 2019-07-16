658ccea15b1968ab399aed643aa1a9ba660d8134
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-16 17:43:15 -0400

    added the prevent default function on submit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+});
+
+let button = document.querySelector('input[type = submit]');
+
+let form = document.querySelector('#create-task-form');
+
+let newTask = document.querySelector('#new-task-description');
+
+let tasks = document.querySelector('#tasks');
+
+form.addEventListener("submit", e => {
+	let newLi = document.createElement('li');
+	newLi.innerHTML = newTask.value;
+	tasks.appendChild(newLi);
+	e.preventDefault();
 });
