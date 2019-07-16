12059ca70b7f9c17934ca5b724ccc0ad2f634d24
Alexander Schelchere <ascellie@gmail.com>
2019-07-16 18:46:45 -0400

    Less work

--- src/index.js
+++ src/index.js
@@ -2,29 +2,42 @@ document.addEventListener('DOMContentLoaded', () => {
   // your code here
   let form = document.querySelector('#create-task-form');
   let tasksList = document.querySelector('#tasks');
-  let input = document.querySelector('#new-task-description');
 
-  let inputVal = '';
+  // let inputVal = '';
 
-  input.addEventListener('input', function(e) {
-    if (e.data == undefined) {
-      // console.log(e.data);
-    } else if (e.data === null) {
-      inputVal = inputVal.slice(0, -1);
-    } else {
-      inputVal += e.data;
-    }
-    // console.log(inputVal);
-  });
+  // input.addEventListener('input', function(e) {
+  //   if (e.data == undefined) {
+  //   } else if (e.data === null) {
+  //     inputVal = inputVal.slice(0, -1);
+  //   } else {
+  //     inputVal += e.data;
+  //   }
+  //   console.log(inputVal);
+  // });
 
   form.addEventListener('submit', function(e) {
-    console.log(e);
-    console.log(e.srcElement);
+    // TY Franchely
+    let input = e.target.querySelector('#new-task-description').value;
+
     let li = document.createElement('li');
-    li.innerText = inputVal;
+    li.innerText = input;
     tasksList.append(li);
-    li.innerHTML += '  <span>❌</span>';
-    inputVal = '';
+    li.innerHTML +=
+      '  <span class="delete-button" style="cursor:pointer;">❌</span>';
     event.preventDefault();
+  });
+
+  let dButton = document.querySelectorAll('.delete-button');
+
+  
+  function deleteTheButton(e) {
+    console.log(e);
+    console.log(e.target);
+    // tasksList.remove(e.target);
+  }
+
+  dButton.forEach(button => {
+    console.log(button);
+    button.addEventListener('click', deleteTheButton);
   });
 });
